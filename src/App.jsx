47e49c2e9b7c29d50e7849ab5a220c9382653b92
Howard <howard@brightpage.tech>
2024-01-25 22:27:16 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import Login from './components/Login'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs(blogs)
+    )
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    const theUser = await loginService.login({ username, password })
+    setUser(theUser)
+    setUsername('')
+    setPassword('')
+  }
+
+  const blogSection = (user) => (
+    <div>
+      < h2 > blogs</h2>
+      <p>{user.name} logged in</p>
+      {
+        blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )
+      }
+    </div>
+  )
+
+  const loginSection = () => (
+    <div>
+      <h2>log in to application</h2>
+      <Login username={username} password={password} setUsername={setUsername} setPassword={setPassword} handleLogin={handleLogin} />
+    </div>
+  )
+
+  return (
+    <div>
+      {!user && loginSection()}
+      {user && blogSection(user)}
+    </div>
+  )
+}
+
+export default App
