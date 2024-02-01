b2d55114ec06134051bf75d2ff2c66fcc6b382be
Howard <howard@brightpage.tech>
2024-02-01 12:05:45 +0000

    Exercise 5.10

--- src/App.jsx
+++ src/App.jsx
@@ -85,7 +85,9 @@ const App = () => {
         <BlogForm createBlog={addBlog} />
       </Toggleable>
       {
-        blogs.map(blog =>
+        blogs
+        .sort((a,b) => b.likes - a.likes)
+        .map(blog =>
           <Blog key={blog.id} blog={blog} blogs={blogs} setBlogs={setBlogs}/>
         )
       }
