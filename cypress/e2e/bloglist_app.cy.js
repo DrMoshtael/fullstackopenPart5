7fb7cfd46ac15ab10245a25bce1d3e80f361bbdd
Howard <howard@brightpage.tech>
2024-02-05 17:33:03 +0000

    Exercise 5.18

--- cypress/e2e/bloglist_app.cy.js
+++ cypress/e2e/bloglist_app.cy.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
+    const user = {
+      username: 'testerX',
+      password: 'pass',
+      name: 'Testing man'
+    }
+    cy.request('POST', `${Cypress.env('BACKEND')}/users`, user)
     cy.visit('')
   })
 
@@ -8,4 +14,26 @@ describe('Blog app', function() {
     cy.contains('username')
     cy.contains('password')
   })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('testerX')
+      cy.get('#password').type('pass')
+      cy.get('#login-button').click()
+
+      cy.contains('Testing man logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('abadf')
+      cy.get('#password').type('pasDfsss')
+      cy.get('#login-button').click()
+
+      cy.get('.notification')
+        .should('contain','Wrong username or password')
+        .should('have.css', 'color', 'rgb(255, 0, 0)')
+      cy.contains('Testing man logged in').should('not.exist')
+    })
+  })
+
 })
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -33,7 +33,7 @@ const LoginForm = ({ logIn }) => {
           required
         />
       </div>
-      <button type="submit">Login</button>
+      <button id='login-button' type="submit">Login</button>
     </form>
   )
 }
